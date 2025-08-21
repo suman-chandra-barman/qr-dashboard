ef3ad158e401c2c83d6da726da78cc5fcd261930
Suman Chandra Barman <sumanchandrabarman999@gmail.com>
2025-08-21 14:47:25 +0600

    Fix overflow issue

--- src/components/layout/MainLayout.tsx
+++ src/components/layout/MainLayout.tsx
@@ -17,7 +17,7 @@ function MainLayout() {
         isCollapsed={isCollapsed}
         setIsCollapsed={setIsCollapsed}
       />
-      <div className="flex-1 flex flex-col overflow-hidden">
+      <div className="flex-1 flex flex-col">
         <div
           className={cn(
             "fixed right-0 left-0 z-10",
@@ -26,7 +26,7 @@ function MainLayout() {
         >
           <Header />
         </div>
-        <main className={cn("mt-20 overflow-y-auto", isCollapsed ? "ml-16" : "ml-64")}>
+        <main className={cn("mt-20", isCollapsed ? "ml-16" : "ml-64")}>
           <Outlet />
         </main>
       </div>
